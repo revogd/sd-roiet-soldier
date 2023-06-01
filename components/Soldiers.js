d000c1d7db9dd2cc53c8651845d0513169b6abcc
revogd <88536930+revogd@users.noreply.github.com>
2023-06-01 10:54:14 +0700

    Update Soldiers.js

--- components/Soldiers.js
+++ components/Soldiers.js
@@ -12,7 +12,7 @@ export default function Soldiers() {
     useEffect(() => {
         const fetchData = async () =>{          
           try {
-            const {data: response} = await Axios.get("https://sd-roiet-api.onrender.com/soldiers");
+            const {data: response} = await Axios.get("https://sd-roiet-api.onrender.com/soldiers?_limit=10");
             
             setRows(response);            
             setLoading(false);
