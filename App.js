3ea91a39fe317342b85e67758a8140ee45ff5d6c
revogd <88536930+revogd@users.noreply.github.com>
2023-06-01 17:31:37 +0700

    WIP 01062023

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Soldiers from './components/Soldiers';
-import Detail from './components/Detail';
+import Soldier from './components/Soldier';
 
 
 
@@ -14,7 +14,7 @@ const Stack = createNativeStackNavigator();
     <NavigationContainer>
       <Stack.Navigator>       
         <Stack.Screen name="Home" component={Soldiers} options={{ title: 'Soldiers' }} />
-        <Stack.Screen name="Detail" component={Detail} />  
+        <Stack.Screen name="Soldier" component={Soldier} />  
       </Stack.Navigator>
     </NavigationContainer>
   );
